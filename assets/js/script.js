ef859fb93748f249470b41cfc5749ab268e22b0a
jd479800 <jd479800@gmail.com>
2023-02-10 20:41:07 -0500

    Creates coordinates request URL based on city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 var inputEl = $('input[name="city-input"]');
 var searchButtonEl = $('#search-button');
+var citySearched = ''
+var searchLimit = 1;
+var apiKey = '984f01ce82f22feac8fdecd70b3ccc45'
 
 searchButtonEl.on('click', function (event) {
     event.preventDefault();
+    citySearched = inputEl.val();
     console.log("City Name: " + inputEl.val());
+    returnCoordinates(citySearched, searchLimit, apiKey);
 });
 
+
+
+function returnCoordinates(citySearched, searchLimit, apiKey) {
+    var coordinatesURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+citySearched+'&limit='+searchLimit+'&appid='+apiKey;
+    console.log(coordinatesURL)
+}
