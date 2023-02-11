0c38676723e753b4d72f442013ad2b5bd1c93183
jd479800 <jd479800@gmail.com>
2023-02-10 20:50:05 -0500

    Returns json and data response

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,27 @@ var inputEl = $('input[name="city-input"]');
 var searchButtonEl = $('#search-button');
 var citySearched = ''
 var searchLimit = 1;
-var apiKey = '984f01ce82f22feac8fdecd70b3ccc45'
+var apiKey = '984f01ce82f22feac8fdecd70b3ccc45';
+var coordinatesURL = '';
 
 searchButtonEl.on('click', function (event) {
     event.preventDefault();
     citySearched = inputEl.val();
     console.log("City Name: " + inputEl.val());
-    returnCoordinates(citySearched, searchLimit, apiKey);
+    coordinatesURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+citySearched+'&limit='+searchLimit+'&appid='+apiKey;
+    console.log(coordinatesURL)
+    returnCoordinates(coordinatesURL);
 });
 
 
 
-function returnCoordinates(citySearched, searchLimit, apiKey) {
-    var coordinatesURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+citySearched+'&limit='+searchLimit+'&appid='+apiKey;
-    console.log(coordinatesURL)
+function returnCoordinates(coordinatesURL) {
+    fetch(coordinatesURL)
+    .then(function (response) {
+      console.log(response);
+      return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+      });
 }
