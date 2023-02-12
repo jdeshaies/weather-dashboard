3a78c3d420606706c27dac98ac5e0ad4fa062bae
jd479800 <jd479800@gmail.com>
2023-02-12 16:48:29 -0500

    Includes max wind speed and humidity level in the object for each day in the forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,9 @@ var currentDayEl = $('#current-date');
 var currentWeatherIconEl = $('#current-weather-icon');
 
 //Variables needed for weather forecast
-var forecastTempArray = [];
+var forecastArray = [];
 var dailyForecastObject = {};
+var fiveDayForecastEl = $('.five-day-forecast');
 
 //Creates a URL to request to cooridinates for the city entered when the search button is clicked
 searchButtonEl.on('click', function (event) {
@@ -92,7 +93,7 @@ function displayCurrentWeather() {
     currentHumidityEl.html('Humidity: ' + currentWeather.humidity.toString() + '%'); 
 }
 
-//LEFT OFF HERE. NOT SURE IF DISPLAYING AVERAGE OF HOURLY FORECASTS FOR 5 DAYS. TURN ON WEATHER FORECAST FUNCTION
+//Creates an object where the key is the date of the five day forecast and the value is the max temp for that day
 function returnWeatherForecast(coordinates) {
   console.log('Start of weather forecast function');
   weatherForecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+coordinates.latitudeValue+'&lon='+coordinates.longitudeValue+'&appid='+apiKey+'&units=imperial';
@@ -104,54 +105,84 @@ function returnWeatherForecast(coordinates) {
   .then(function (data) {
       console.log(data);
       var listLength = data.list.length
-      console.log('List length: ' + listLength);
+      // console.log('List length: ' + listLength);
       lastValue = listLength-1;
-      console.log('One less than list length: ' + lastValue);
+      // console.log('One less than list length: ' + lastValue);
       var maxTemp = 0;
+      var maxWindSpeed = 0;
+      var maxHumidity = 0;
       var objectCounter = 0;
       var firstDay = true;
       for (var i=0; i<data.list.length; i++) {
-        console.log('Last Value: ' + lastValue);
-        console.log('i: ' + i);
+        // console.log('Last Value: ' + lastValue);
+        // console.log('i: ' + i);
         var splitArray = data.list[i].dt_txt.split(' ');
         var forecastDate = splitArray[0];
         if (i != 0) {
           var splitArray2 = data.list[i-1].dt_txt.split(' ');
           var prevDay = splitArray2[0];
         }
-        var temp = data.list[i].main.temp
+        var forecastTemp = data.list[i].main.temp;
+        var forecastWindSpeed = data.list[i].wind.speed;
+        var forecastHumidity = data.list[i].main.humidity;
         if (forecastDate !== dayjs().format('YYYY-MM-DD').toString()) {
-          console.log('Loop #: ' + i);
-          console.log('Forecast Date: ' + forecastDate);
-          console.log('Prev Day: ' + prevDay);
+          // console.log('Loop #: ' + i);
+          // console.log('Forecast Date: ' + forecastDate);
+          // console.log('Prev Day: ' + prevDay);
           if (firstDay) {
-            console.log('First iteration');
-            maxTemp = temp;
+            // console.log('First iteration');
+            maxTemp = forecastTemp;
+            maxWindSpeed = forecastWindSpeed;
+            maxHumidity = forecastHumidity;
             firstDay = false;
           }
           else if (i === lastValue) {
-            console.log('Last Iteration');
-            dailyForecastObject[objectCounter] = {date: forecastDate, highTemp: maxTemp};
+            // console.log('Last Iteration');
+            dailyForecastObject[objectCounter] = {
+              date: forecastDate, 
+              topTemp: maxTemp,
+              topWindSpeed: maxWindSpeed,
+              topHumidity: maxHumidity
+            };
           }
           else if (prevDay === forecastDate) {
-            console.log('Prev day the same');
-            if (temp > maxTemp) {
-              console.log('New max temp of: ' + maxTemp);
-              maxTemp = temp;
-            }
+            // console.log('Prev day the same');
+            if (forecastTemp > maxTemp) {
+              // console.log('New max temp of: ' + maxTemp);
+              maxTemp = forecastTemp;
+            };
+            if (forecastWindSpeed > maxWindSpeed) {
+              // console.log('New max temp of: ' + maxTemp);
+              maxWindSpeed = forecastWindSpeed;
+            };
+            if (forecastHumidity > maxHumidity) {
+              // console.log('New max temp of: ' + maxTemp);
+              maxHumidity = forecastHumidity;
+            };
           }
           else {
-            console.log('New day');
-            dailyForecastObject[objectCounter] = {date: prevDay, highTemp: maxTemp};
+            // console.log('New day');
+            dailyForecastObject[objectCounter] = {
+              date: prevDay, 
+              topTemp: maxTemp,
+              topWindSpeed: maxWindSpeed,
+              topHumidity: maxHumidity
+            };
             maxTemp = 0;
+            maxWindSpeed = 0;
+            maxHumidity = 0;
             objectCounter++;
-            console.log(dailyForecastObject);
+            // console.log(dailyForecastObject);
           }
-          console.log('Temp: ' + temp);
-          console.log('Max Temp: ' + maxTemp);
+          // console.log('Temp: ' + temp);
+          // console.log('Max Temp: ' + maxTemp);
         }
       }
-      forecastTempArray.push(dailyForecastObject);
-      console.log(forecastTempArray);
+      forecastArray.push(dailyForecastObject);
+      console.log(forecastArray);
     });
+}
+
+function displayFiveDayForecast() {
+
 }
